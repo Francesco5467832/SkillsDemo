7bdde0bd11206cada0d15abc0d90aa1ac4fa4b5e
Francesco5467832 <129178052+Francesco5467832@users.noreply.github.com>
2023-04-17 19:12:05 +0200

    Add files via upload

---
+++ SkillsDemo/SkillsDemo/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkillsDemo
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //Beispiel für alle Skills eines Mitarbeiters
+        public Employee employee = new Employee();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            for(int i = 0;i <10;i++)
+            {
+                Skill skill = new Skill(5,"C#", SkillLevel.Amateur, 1);
+                employee.Skills.Add(skill);
+            }
+
+            dg_Skills.DataContext = employee.Skills;
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            //Selected item nehmen
+
+            //aus db löschen
+            //aus liste lokal löschen
+
+            //Wenn die Ansicht nicht updated dann:
+            //dg_Skills.DataContext = null;
+            //dg_Skills.DataContext = employee.skills;
+        }
+
+        //Alternative zum Datagrid alleine
+        //Datagrid auf nicht editierbar stellen und einzelne Textboxen nehmen und mit Daten vom asugewählten Objekt füllen
+        //ICollectionView ist gut für die Liste (kein muss)
+    }
+}
---
+++ SkillsDemo/SkillsDemo/Skill.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsDemo
+{
+    public enum SkillLevel
+    {
+        Anfänger = 1,
+        Amateur,
+        Fortgeschritten,
+    }
+
+    public class Skill
+    {
+        public int Skill_Id { get; set; }
+        public string Skill_name { get; set; }
+        public SkillLevel Skill_level { get; set; }
+        public int Employee_id { get; set; }
+
+        public Skill(int skill_Id, string skill_name, SkillLevel skill_level, int employee_id)
+        {
+            Skill_Id = skill_Id;
+            Skill_name = skill_name;
+            Skill_level = skill_level;
+            Employee_id = employee_id;            
+        }
+    }
+
+    public class Employee
+    {
+        public int Employee_id { get; set; }
+        public string Employee_name { get; set; }
+
+        public List<Skill> Skills = new List<Skill>();
+    }
+}
